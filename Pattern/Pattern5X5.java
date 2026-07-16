d62eb0735a625f4fcba731a69e10dc9bcad5e6f2
muthukrishnanr19 <muthukrishnanr19@gmail.com>
2026-07-16 22:42:25 +0530

    5*5 pattern

---
+++ Pattern/Pattern5X5.java
@@ -0,0 +1,10 @@
+public class Pattern5X5 {
+    public static void main(String[] args) {
+        String patt="*";
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(patt);
+            }
+            System.out.println();
+        }
+}}
