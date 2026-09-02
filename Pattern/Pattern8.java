31e00d67f192ee18edf3e39d083df5ebb02c61f5
muthukrishnanr19 <muthukrishnanr19@gmail.com>
2026-09-02 11:37:28 +0530

    pattern 8 and 9

--- Pattern/Pattern8.java
+++ Pattern/Pattern8.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-
-public class Pattern7 {
+public class Pattern8 {
     public static void main(String[] args) {
         int num=5;
         for(int i=1;i<=num;i++){
-            for(int j=0;j<num-i;j++){
+            for(int j=1;j<i;j++){
                 System.out.print(" ");    
             }
-            for(int k=0;k<2*i-1;k++){
+            for(int k=0;k<2*num-(2*i-1);k++){
                 System.out.print("*");    
             }
             System.out.println();
--- Pattern/Pattern9.java
+++ Pattern/Pattern9.java
@@ -1,6 +1,4 @@
-import java.util.Scanner;
-
-public class Pattern7 {
+public class Pattern9 {
     public static void main(String[] args) {
         int num=5;
         for(int i=1;i<=num;i++){
@@ -8,6 +6,16 @@ public class Pattern7 {
                 System.out.print(" ");    
             }
             for(int k=0;k<2*i-1;k++){
+                System.out.print("*");    
+            }
+            System.out.println();
+        }
+        
+        for(int i=1;i<=num;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");    
+            }
+            for(int k=0;k<2*num-(2*i-1);k++){
                 System.out.print("*");    
             }
             System.out.println();
